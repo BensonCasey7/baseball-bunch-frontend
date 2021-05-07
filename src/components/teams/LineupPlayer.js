581d0fbc1e27e41eeaa8a2672155206efc5a77e0
Bensoncasey7 <bensoncasey7@gmail.com>
2021-05-06 21:56:49 -0500

    Updated team page to pull in player names rather than IDs

--- src/components/teams/LineupPlayer.js
+++ src/components/teams/LineupPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BsPencil } from "react-icons/bs";
 import { FiSave } from "react-icons/fi";
@@ -6,9 +6,31 @@ import { ImCancelCircle } from "react-icons/im";
 
 import Typeahead from "../players/Typeahead";
 import positionAbbreviations from "../../utils/positionAbbreviations";
+import Loading from "../shared/Loading";
 
 const LineupPlayer = (props) => {
   const [editingPlayer, setEditingPlayer] = useState(false);
+  const [fullPlayerData, setFullPlayerData] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    fetch(
+      `https://cs411baseball.web.illinois.edu/api/player/${props.playerid}`,
+      requestOptions
+    ).then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+        setFullPlayerData(data);
+        setLoaded(true);
+      }
+    });
+  }, [props.playerid]);
 
   const updatePlayer = (e) => {
     props.setDirtyPlayers({
@@ -54,15 +76,24 @@ const LineupPlayer = (props) => {
           </div>
         </form>
       ) : (
-        <div key={props.position} className={"lineup-player"}>
-          <Link to={`/players/${props.playerid}`}>{props.playerid}</Link>
-          <BsPencil
-            onClick={() => {
-              setEditingPlayer(true);
-            }}
-            className={"lineup-player__action"}
-          />
-        </div>
+        <>
+          {loaded ? (
+            <div key={props.position} className={"lineup-player"}>
+              <Link
+                to={`/players/${props.playerid}`}
+                style={{ whiteSpace: "nowrap" }}
+              >{`${fullPlayerData.namefirst} ${fullPlayerData.namelast}`}</Link>
+              <BsPencil
+                onClick={() => {
+                  setEditingPlayer(true);
+                }}
+                className={"lineup-player__action"}
+              />
+            </div>
+          ) : (
+            <Loading className={"loading--tiny"} />
+          )}
+        </>
       )}
     </>
   );
